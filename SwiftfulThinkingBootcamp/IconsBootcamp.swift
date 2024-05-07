c81ef15782326a029ce2c334085197f0a8a4e719
enesozmus <ensozmus@gmail.com>
2024-05-07 16:52:51 +0300

    🔨 Icons 2

--- SwiftfulThinkingBootcamp/IconsBootcamp.swift
+++ SwiftfulThinkingBootcamp/IconsBootcamp.swift
@@ -30,6 +30,35 @@ struct IconsBootcamp: View {
         Image(systemName: "xmark")
         Image(systemName: "person.badge.plus")
             .renderingMode(.original)
+        Image(systemName: "theatermasks")
+            .symbolRenderingMode(.hierarchical)
+            .foregroundStyle(.blue)
+        Image(systemName: "person.badge.plus")
+            .symbolRenderingMode(.palette)
+            .foregroundStyle(.green, .blue)
+        Image(systemName: "shareplay")
+            .symbolRenderingMode(.palette)
+            .foregroundStyle(.blue, .black)
+        Image(systemName: "person.3.sequence.fill")
+            .symbolRenderingMode(.palette)
+            .foregroundStyle(.blue, .red, .green)
+        Image(systemName: "person.3.sequence.fill")
+            .symbolRenderingMode(.palette)
+            .foregroundStyle(
+                .linearGradient(colors: [.red, .black], startPoint: .top, endPoint: .bottomTrailing),
+                .linearGradient(colors: [.green, .black], startPoint: .top, endPoint: .bottomTrailing),
+                .linearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottomTrailing)
+            )
+            .font(.system(size: 144))
+        // cmd + shift + l
+        Image(systemName: "figure.jumprope")
+        
+        Image(systemName: "battery.100.bolt")
+            .resizable()
+            .symbolRenderingMode(.palette)
+            .foregroundStyle(.brown, .gray, .green)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 100, height: 100)
     }
 }
 
