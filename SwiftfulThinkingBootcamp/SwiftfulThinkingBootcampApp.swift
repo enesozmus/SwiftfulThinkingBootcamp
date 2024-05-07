81cde6c3cdf2f4be6ee5c3f1c306167acd05ee4a
enesozmus <ensozmus@gmail.com>
2024-05-07 11:37:21 +0300

    Initial Commit

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcampApp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by enesozmus on 7.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
