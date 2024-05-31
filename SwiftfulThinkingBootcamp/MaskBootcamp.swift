82d3e81358ecdd6edc035ca37d281fe4e031dca1
enesozmus <ensozmus@gmail.com>
2024-05-31 17:32:14 +0300

    🔨 Mask

---
+++ SwiftfulThinkingBootcamp/MaskBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  MaskBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by enesozmus on 31.05.2024.
+//
+
+import SwiftUI
+
+struct MaskBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MaskBootcamp()
+}
